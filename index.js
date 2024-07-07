fedf67a7549df86466dc8c07deb4eb511aae996e
k-ahsan07 <100510506+k-ahsan07@users.noreply.github.com>
2024-07-07 18:26:02 +0500

    changed

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const app = express()
-const mongoose = require("mongoose")
+// const express = require('express')
+// const app = express()
+// const mongoose = require("mongoose")
 
-mongoose.connect("mongodb+srv://ahsan07:ahsan2003@cluster0.x1tvd1q.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
-).then(()=>console.log("Db connection successful ")).catch((err)=>{
-    console.log(err)
-});
+// mongoose.connect("mongodb+srv://ahsan07:ahsan2003@cluster0.x1tvd1q.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
+// ).then(()=>console.log("Db connection successful ")).catch((err)=>{
+//     console.log(err)
+// });
 
-app.listen(5000, ()=>{
-    console.log("backend server is running")
+// app.listen(5000, ()=>{
+//     console.log("backend server is running")
 
-});
+// });
+
+
+console.log("PAksissdgsd")
