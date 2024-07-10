a23f5b46d7f1fa4665e859cc79727f939b12283e
k-ahsan07 <100510506+k-ahsan07@users.noreply.github.com>
2024-07-10 16:48:40 +0500

    pak

--- routers/auth.js
+++ routers/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // Register
 router.post("/register", async (req, res) => {
@@ -37,13 +38,18 @@ router.post("/login", async (req, res) => {
     }
 
     // jwt
-    // const accesstoken = jwt.sign({
-    //   id: user._id, isAdmin: user.isAdmin
-    // }, process.env.JWT_SEC, { expiresIn: "3d" });
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
 
     const { password, ...others } = user._doc;
 
-    res.status(200).json({ ...others });
+    res.status(200).json({ id: user._id, ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
--- routers/users.js
+++ routers/users.js
@@ -2,26 +2,37 @@ const router = require("express").Router();
 const {verifyToken,verifyTokenAndAuthorization} = require("./verifyToken")
 const CryptoJS = require("crypto-js");
 
-router.put("/:id",verifyToken, async (req,res)=>{
-    // if(req.body.password){
-    //     req.body.password = CryptoJS.AES.encrypt(
-    //         req.body.password,
-    //         process.env.PASS_SEC
-    //       ).toString()
-    // }
-    // try {
-    //     const updatedUser = await User.findByIdAndUpdate(
-    //       req.params.id,
-    //       {
-    //         $set: req.body,
-    //       },
-    //       { new: true }
-    //     );
-    //     res.status(200).json(updatedUser);
-    //   } catch (err) {
-    //     res.status(500).json(err);
-    //   }
-    });
 
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+      req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+      ).toString();
+    }
+  
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  //DELETE
+  router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been deleted...");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router
